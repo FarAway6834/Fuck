274a8b1b8d27f03b7e762f9ec376a2d6dacf56ed
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-22 16:29:44 +0900

    정신차림

--- prototype.py
+++ prototype.py
@@ -1,5 +1,25 @@
-#cmd만 아는 병신~~ㅋ 뒤져 씨발놈아
-#뭘안다고 까뿌러
-#아씨발 왜 생일에 계네들이 수근댈거 의식허눈지
-
-class limit
+class limit_bit:#수요자가너무병신이라잠시동안병신으로만들어놓겠음
+    __slots__ = ('__sbj', '__bit', '__max')
+    def __init__(self, *target, bit):
+        try: assert len(target)==1
+        except:
+            raise AttibutrError('limit_bit has only 1 potional arguemnts and only 1 keyworld argument `bit`')
+        2pb = 2**bit
+        self.__2pb = 2pb
+        self.__max = 2pb-1
+        self.__sbj = target[0]
+        self.__bit = bit
+    
+    __invert__ = lambda self : self.__max - self
+    
+    def __and__(self, x):
+        #대충 index바로제서 하는걸로 구성할것.
+        #or도, xor도
+        return ...
+    
+    def __lshift__(self, x):
+        return (2**x)*self - self.__2pb*(x//(2**(self.__bit-x)))
+    
+    __rshift__ = lambda self, x : self // (2 ** x)
+    
+    #잠깐만씨발내가왜이런병신같고개쉬운프로젝트가지고지랄햐여하는데
